3eb36d4119cd8310f314afe7366e881b983baf7f
ZETHYR\zzWeeZz <zzWeeZznice@gmail.com>
2024-12-08 00:19:52 +0100

    Sharpmake now builds the solutions the right way. This needs to be configured.

--- Build_Solution.bat
+++ Build_Solution.bat
@@ -1,24 +1,28 @@
 @echo off
-pushd Vendor
-pushd Sharpmake
+
 :: Clear previous run status
 COLOR
 
-:: First compile sharpmake to insure we are trying to deploy using an executable corresponding to the code.
-dotnet build Sharpmake.sln /p:Configuration=Release /p:Platform="Any CPU"
-if %errorlevel% NEQ 0 goto error
+set SHARPMAKE_EXECUTABLE=%~dp0Vendor\Sharpmake\Sharpmake.Application\bin\Release\net6.0\Sharpmake.Application.exe
 
+
+pushd Vendor
+pushd Sharpmake
+:: First compile sharpmake to insure we are trying to deploy using an executable corresponding to the code.
+if not exist %SHARPMAKE_EXECUTABLE% dotnet build Sharpmake.sln /p:Configuration=Release /p:Platform="Any CPU"
+if %errorlevel% NEQ 0 goto error
+popd
+popd
 
 echo sharpmake executable is built, fetching executable...
-set SHARPMAKE_EXECUTABLE=%~dp0Sharpmake.Application\bin\Release\net6.0\Sharpmake.Application.exe
-if not exist %SHARPMAKE_EXECUTABLE% echo Cannot find sharpmake executable in %~dp0Sharpmake.Application\bin\Release\net6.0 & pause & goto error
 
-popd
-popd
+if not exist %SHARPMAKE_EXECUTABLE% echo Cannot find sharpmake executable in %~dp0%~dp0Vendor\Sharpmake\Sharpmake.Application\bin\Release\net6.0 & pause & goto error
+
+
 
 echo sharpmake building solution...
 
-call %SHARPMAKE_EXECUTABLE% /sources(ProjectDefinitions/Solution.cs)
+call %SHARPMAKE_EXECUTABLE% /sources('ProjectDefinitions/main.sharpmake.cs')
 
 pause
 
